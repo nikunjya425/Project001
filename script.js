a25743b2dcaed456570c3dc97fa77f9fd86442db
nikunjya425 <Kumarnikunjya@gmail.com>
2025-03-21 13:13:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+
+    // Add smooth scroll animation for all internal links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
